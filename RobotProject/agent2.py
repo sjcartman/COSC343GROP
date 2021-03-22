041fc8955fde8481f53bd862535fcd6c7bfb296e
taya-nicholas <80496709+taya-nicholas@users.noreply.github.com>
2021-03-23 09:28:16 +1300

    Add files via upload

---
+++ RobotProject/agent2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.sound import Sound
+from ev3dev2.sensor.lego import ColorSensor
+from framework2 import GoalAgent
+import time
+
+ga = GoalAgent()
+
+ga.calibrate_forward()
+ga.calibrate_forward()
+ga.calibrate_forward()
+
+print("rotation 1")
+ga.calibrate_rotation()
+print("rotation 2")
+ga.calibrate_rotation()
+print("rotation 3")
+ga.calibrate_rotation()
+print("rotation 4")
+ga.calibrate_rotation()
+
+
+
+##while not ga.goal_found:
+   ## ga.action(ga.rule())
+
+time.sleep(2)
+
