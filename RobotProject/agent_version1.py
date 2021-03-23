68435597a6349f6358fbb2dd8bc68a13836f5ced
Sean Cartman <casea836@student.otago.ac.nz>
2021-03-23 13:55:35 +1300

    new files with unix line endings

--- RobotProject/agent_version1.py
+++ RobotProject/agent_version1.py
@@ -11,6 +11,7 @@ mRight = LargeMotor(OUTPUT_C)
 cs = ColorSensor()
 drive = MoveTank(OUTPUT_B, OUTPUT_C)
 flip = True
+turn_left_on_grey = false
 count = 0
 ga = GoalAgent()
 
@@ -19,17 +20,21 @@ ga.var_forward(0.8)
 ga.right()
 while True:
     drive.on(SpeedPercent(20), SpeedPercent(20))
-    if (cs.color == 1 and flip) or (cs.color == 6 and not flip):
+    if (cs.reflected_light_intensity <= 10 and flip) or (cs.reflected_light_intensity >= 90 and not flip):
         drive.off()
         time.sleep(.5)
         flip = not flip
         if flip:
             count = count +1
-    if(count == 13):
+    elif(count == 13):
         drive.off()
         time.sleep(1)
         ga.right()
         count = count + 16
+    if (cs.reflected_light_intensity > 10 and cs.reflected_light_intensity < 90) :
+        drive.off()
+        time.sleep(.5)
+
 
 
 for i in range(1,12):
---
+++ RobotProject/testAgent2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.sound import Sound
+from ev3dev2.sensor.lego import ColorSensor
+from f2 import GoalAgent
+import time
+
+ga = GoalAgent()
+
+ga.calibrate_forward()
+ga.calibrate_forward()
+ga.calibrate_forward()
+
+# rotation calls. Each rotation call should move through white to black, and then through black to the next white. But there is currently trouble at the end to resolve.s
+print("rotation 1")
+ga.calibrate_rotation()
+print("rotation 2")
+ga.calibrate_rotation()
+print("rotation 3")
+ga.calibrate_rotation()
+print("rotation 4")
+ga.calibrate_rotation()
+
+
+
+##while not ga.goal_found:
+   ## ga.action(ga.rule())
+
+time.sleep(2)
+
